d54ea3e494a8a9552e761609dd965a9b0225ca22
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2023-01-28 11:53:43 -0300

    aula 41

---
+++ aula41/main.js
@@ -0,0 +1,10 @@
+//Escreva uma função que receba 2 números e retorna o maior deles
+
+function maior (a, b) {
+    return a > b ? a : b;
+}
+
+const max = (a, b) => a > b ? a : b;
+
+console.log(maior(3,2));
+console.log(max(1,2));
