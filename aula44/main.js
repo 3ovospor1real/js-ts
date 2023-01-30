5e5aaa95def46a97febbfa49cdef42e18b0d4d0b
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2023-01-30 20:04:56 -0300

    aula 44

---
+++ aula44/main.js
@@ -0,0 +1,18 @@
+
+function soma (x, y) {
+    if(
+        typeof x !== 'number' ||
+        typeof y !== 'number'
+    ) {
+        throw new Error('x e y precisam ser números.');
+    }
+
+    return x + y;
+}
+
+try {
+    console.log(soma(1,2));
+    // console.log(soma('1', 2));
+} catch (error) {
+    console.log('Ocorreu um erro.')
+}
