0d2a429325dbb1384b6562c1f57d379adaf8e492
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2023-01-31 12:38:18 -0300

    aula 45

---
+++ aula45/main.js
@@ -0,0 +1,19 @@
+function retornaHora(data) {
+    if(data && !(data instanceof Date)){
+        throw new TypeError('Esperando instância Date');
+    }
+
+    if(!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    })
+}
+
+const hora = new Date('2007-12-27');
+console.log(hora.toLocaleString('en-US'));
