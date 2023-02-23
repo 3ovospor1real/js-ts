9115a46582da7ba0df289b9b7bacb8fbb07f9ca5
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2023-02-23 18:44:13 -0300

    aula 62

---
+++ aula62 - Revisão Arrays/main.js
@@ -0,0 +1,25 @@
+/*
+// Forma literal
+const nomes = new Array('João', 'Miguel', 'Nívia');
+
+//Remove sem alterar o índice
+delete nomes[1];
+
+
+console.log(nomes);
+*/
+
+/*
+const nomes = new Array('João', 'Miguel', 'Nívia');
+
+// Spread operator, espalha os itens do array no novo array
+const novo = [...nomes];
+
+console.log(nomes);
+console.log(novo);
+*/
+
+let nome = 'João Humberto Ribeiro'
+let array = nome.split(' ')
+
+console.log(array)
