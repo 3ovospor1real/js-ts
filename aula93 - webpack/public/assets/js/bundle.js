6c64af0fc35ea44e366967107e16073a45b221ed
João H. A. Ribeiro <113396206+3ovospor1real@users.noreply.github.com>
2024-01-23 20:26:20 -0300

    aula

---
+++ aula93 - webpack/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var myName = 'João';
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
